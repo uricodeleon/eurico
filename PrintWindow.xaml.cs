637ec2e7c8a6e9fc675392fd844bac5d9fecce22
uricodeleon <31833231+uricodeleon@users.noreply.github.com>
2017-10-25 13:38:10 +0800

    Add files via upload

---
+++ PrintWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using System.Windows.Markup;
+using System.Data;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace Inventory_1
+{
+    /// <summary>
+    /// Interaction logic for PrintWindow.xaml
+    /// </summary>
+    public partial class PrintWindow : MetroWindow
+    {
+
+       
+        public PrintWindow()
+        {
+         
+            InitializeComponent();
+          
+        }
+
+        public void display()
+        {
+           
+        }
+        private void FixedDocument_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        private void print_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+         
+         
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+           
+           
+
+        }
+    }
+   
+
+}
---
+++ Transaction.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+namespace Inventory_1
+{
+    /// <summary>
+    /// Interaction logic for Transaction.xaml
+    /// </summary>
+    public partial class Transaction : MetroWindow
+    {
+        List<string> list = new List<string>();
+
+        public Transaction()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void searchTxtBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+                PrintDialog printDialog = new PrintDialog();
+                if (printDialog.ShowDialog() == true)
+                {
+                    printDialog.PrintVisual(dataGridTranscaction, "My First Print Job");
+                }
+            
+        }
+    }
+}
+
